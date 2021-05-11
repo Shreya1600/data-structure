4ab125f6e3e676a85c9909bfbf03146169ecd114
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2021-05-12 01:03:15 +0530

    Add files via upload

---
+++ PRODUCT OF TWO NUMBERS USING RECURSION.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int product(int,int);
+int main(void)
+{
+    int a,b,result;
+
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    result=product(a,b);
+    printf("Product of %d and %d is %d\n",a,b,result);
+    return 0;
+}
+int product(int a,int b)
+{
+    if(a>b)
+    {
+        return product(b,a);
+    }
+    else if(b!=0)
+    {
+        return (a+product(a,b-1));
+    }
+    else
+    {
+        return 0;
+    }
+}
